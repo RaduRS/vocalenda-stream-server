83a3b16d806b0febb5dcc71a7dc48ecbdc8d0f60
RaduRS <48391644+RaduRS@users.noreply.github.com>
2025-09-18 19:58:57 +0100

    fine tune bye system

--- deepgram.js
+++ deepgram.js
@@ -1003,7 +1003,7 @@ async function handleDeepgramMessageType(deepgramData, timestamp, context) {
             JSON.stringify({
               type: "InjectAgentMessage",
               content:
-                "I notice you've been quiet for a while. Thank you for calling! Have a great day and goodbye! [CALL end_call FUNCTION NOW]",
+                "I notice you've been quiet for a while. Thank you for calling! Have a great day and goodbye!",
             })
           );
 
